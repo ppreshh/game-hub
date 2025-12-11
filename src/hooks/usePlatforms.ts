dd3282f45d285369aaa0833e643b5287e727b38b
presh <naidu.s.prashant@gmail.com>
2025-12-10 21:08:27 -0800

    update page title

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,11 @@
 import platforms from "@/data/platforms";
-import useData from "./useData";
+//import useData from "./useData";
 
-interface Platform{
-    id: number;
-    name: string;
-    slug: string;
-}
+// interface Platform{
+//     id: number;
+//     name: string;
+//     slug: string;
+// }
 
 const usePlatforms = () => ({data: platforms.results, isLoading: false, error: null})
 //const usePlatforms = () => useData<Platform>("/platforms/lists/parents")
